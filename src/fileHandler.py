2a29d08121062d8f243f077bc1dbf52d303a5a6c
Connor  Keevill <connor.keevill.19@gmail.com>
2016-10-22 17:12:31 +0100

    Add fileHandler content

--- src/fileHandler.py
+++ src/fileHandler.py
@@ -1,2 +1,37 @@
 #CK
 
+class fileHandler():
+
+    def __init__(self, fileName=None):
+
+        if fileName == None:
+            self.fileName = self.getFileName()
+        else:
+            self.fileName = fileName
+
+        self.file = None
+
+    def getFileName(self):
+        prompt = 'Please enter the directory of the file you\'re using: '
+
+        while True:
+            try:
+                name = input(prompt)
+                newFile = open(name)
+                newFile.close()
+                break
+            except:
+                print('FAILED')
+                print('Make sure the file you\'re attempting to open exists and that you have the correct file path.')
+        return name
+
+    def addWrite(self, message):
+        self.file = open(self.fileName, 'a')
+
+    def overWrite(self, message):
+        self.file = open(self.fileName, 'w')
+        self.write(message)
+
+    def write(self, message):
+        self.file.write(message)
+        self.file.close()
