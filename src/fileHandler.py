6f13b8b67933417237954ad3913bc063335b1d57
Connor  Keevill <connor.keevill.19@gmail.com>
2016-10-23 22:44:13 +0100

    Add documentation for fileHandler

--- src/fileHandler.py
+++ src/fileHandler.py
@@ -1,7 +1,17 @@
 #CK
 
+# | fileHandler()
+# |--------------------------------------------------------------------------------------
+# | Object to handle any file IO. This is created as a wrapper for Python's file object
+# | to bundle everything into an object which is easier to manage. The fileHandler()
+# | object acts as a file which remains open, but actually operates by storing a
+# | file name, and opening that file in the required mode every time data is
+# | written or appended to it, or read from it. This allows methods like
+# | file.add() to be used in parallel with functions like file.read()
+# | without having to worry about closing and reopening the file
+# | in a different mode, making it easier to interact with it.
+# |-------------------------------------------------------
 class fileHandler():
-
     def __init__(self, fileName=None):
 
         if fileName == None:
@@ -11,6 +21,13 @@ class fileHandler():
 
         self.file = None
 
+    # | getFileName()
+    # |-----------------------------------------------------
+    # | If the object was initialised without a file name,
+    # | this method prompts the user to enter one, and
+    # | ensures that the entered directory is valid
+    # | with a simple 'while, try, except' loop.
+    # |-------------------------------------
     def getFileName(self):
         prompt = 'Please enter the directory of the file you\'re using: '
 
@@ -25,14 +42,32 @@ class fileHandler():
                 print('Make sure the file you\'re attempting to open exists and that you have the correct file path.')
         return name
 
-    def addWrite(self, message):
+    # | append()
+    # |-----------------------------------------------
+    # | Assumes there is no file open and opens the
+    # | file in 'a' mode, then calls the class'
+    # | write method with the message param.
+    # |---------------------------------
+    def append(self, message):
         self.file = open(self.fileName, 'a')
         self.write(message)
 
+    # | overWrite()
+    # |-----------------------------------------------
+    # | Assumes there is no file open and opens the
+    # | file in 'w' mode, then calls the class'
+    # | write method with the message param.
+    # |---------------------------------
     def overWrite(self, message):
         self.file = open(self.fileName, 'w')
         self.write(message)
 
+    # | write()
+    # |-----------------------------------------------
+    # | Assumes there is a file open, and calls the
+    # | file's write() method using the message
+    # | parameter, before closing the file.
+    # |--------------------------------
     def write(self, message):
         self.file.write(message)
         self.file.close()
