5ec8a38ef5a34f19a31626d908a09c8680e8441f
Connor  Keevill <connor.keevill.19@gmail.com>
2016-10-25 22:08:43 +0100

    Change Password() object into Account()

---
+++ src/Account.py
@@ -0,0 +1,39 @@
+#CK
+
+import stringHandler
+
+# | Account() - previously Password()
+# |-----------------------------------------------------------------------------------------------------
+# | Object for an account. Contains attributes for the account type, username, plaintext password and
+# | and encrypted password, and if it's a new or one read from a file. It also contains methods for
+# | encrypting and decrypting the password, depending on if the account is new or being re-read.
+# |-----------------------------------------------------------------------------------------
+class Account:
+    def __init__(self, new, accountType, username, password):
+        self.accountType = accountType
+        self.username = username
+
+        if new:
+            self.password = password
+            self.encrypted = None
+        else:
+            self.encrypted = password
+            self.password = None
+
+        self.new = new
+
+    # | encrypt()
+    # |-------------------------------------------------------------------------------------
+    # | Calls the encrypt method from stringHandler on the password's plaintext attribute.
+    # |-------------------------------------------------------------------------------
+    def encrypt(self, keyword):
+        self.encrypted = stringHandler.encrypt(self.password, keyword)
+        print(self.encrypted)
+
+    # | decrypt()
+    # |--------------------------------------------------------------------------------------
+    # | Calls the decrypt method from stringHandler on the password's encrypted attribute.
+    # |--------------------------------------------------------------------------------
+    def decrypt(self, keyword):
+        self.password = stringHandler.decrypt(self.encrypted, keyword)
+        print(self.password)
