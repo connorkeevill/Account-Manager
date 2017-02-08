ac14480737fdf966327d14edcb487ecf7a362536
connorkeevill12 <connor.keevill.19@gmail.com>
2017-02-08 10:56:05 +0000

    Add explanation for main.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,10 @@
 from Account import Account
 from fileHandler import fileHandler
 
+# | Currently a file to create a new account object with new data from the user
+# | and write it to the passwords.txt file. Certain parameters are hard coded
+# | for now to test specific aspects.
+
 word = input('Enter your password: ')
 account = Account(1, word)
 file = fileHandler()
