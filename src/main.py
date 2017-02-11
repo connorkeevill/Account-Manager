8f00295fb637bdc66375217a0e50d76620994766
Connor  Keevill <connor.keevill.19@gmail.com>
2017-02-11 20:23:37 +0000

    Move code in helpers.py to templates.py

--- src/main.py
+++ src/main.py
@@ -2,25 +2,11 @@
 
 from Account import Account
 from FileHandler import FileHandler
-import helpers
+import templates
 
 file = FileHandler('accounts.txt')
 
 for account in file.accounts:
-    helpers.displayAccountDetails(account.type, account.owner, account. username)
+    templates.displayAccountDetails(account.type, account.owner, account. username)
 
 
-"""
-decision = input('Do you want to [V]iew existing accounts or [A]dd a new one? ').upper()
-
-if decision == 'V':
-    for account in file.accounts:
-        print(account.type)
-        account.decrypt('!')
-elif decision == 'A':
-    account = Account(1, 'account4', 'username4', 'password')
-    account.encrypt('!')
-    file.addAccount(account)
-else:
-    print('Invalid choice, exiting')
-"""
