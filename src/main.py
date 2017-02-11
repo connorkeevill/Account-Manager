e73458d03e0647240ac0d856246b2a94bcb5bb77
Connor  Keevill <connor.keevill.19@gmail.com>
2017-02-11 21:11:13 +0000

    Add method for welcome message in templates.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,15 @@ import templates
 
 file = FileHandler('accounts.txt')
 
+#Insert code for selection of programs function:
+
+
+
+
+#Displaying accounts w/out password
 for account in file.accounts:
     templates.displayAccountDetails(account.type, account.owner, account. username)
+
+
 
 
--- src/templates.py
+++ src/templates.py
@@ -1,5 +1,16 @@
 #CK
 
+# | displayGreeting()
+# |------------------------------------------------------------
+# | Displays message to be shown on the program being opened.
+# |--------------------------------------------------------
+def displayGreeting():
+    print('Welcome') #(to be improved)
+
+# | displayAccountDetails()
+# |---------------------------------------------------------------------------------
+# | Template to show structured layout of 'non-confidential' details of an account.
+# |-----------------------------------------------------------------------------
 def displayAccountDetails(type, owner, username):
     print('===================================================================\n',
           type,'-', owner, '\n',
