ce8a49c50ce6911fc0e48b8d4dbc9c214b145c7f
Connor  Keevill <connor.keevill.19@gmail.com>
2017-02-14 23:06:00 +0000

    Add greeting() and mainMenu() methods to templates Rename templates.displayAccountDetails() to just accountDetails()

--- src/templates.py
+++ src/templates.py
@@ -1,17 +1,28 @@
 #CK
 
-# | displayGreeting()
-# |------------------------------------------------------------
-# | Displays message to be shown on the program being opened.
-# |--------------------------------------------------------
-def displayGreeting():
-    print('Welcome') #(to be improved)
+# | greeting()
+# |-----------------------------------------------------------
+# | Returns message to be shown on the program being opened.
+# |-------------------------------------------------------
+def greeting():
+    return('===================================================================\n'
+          'Welcome\n'
+          '===================================================================\n')
 
-# | displayAccountDetails()
+# | mainMenu()
+# |-------------------------------------------------------------------------
+# | Returns message to prompt user on choice for next action in main menu.
+# |--------------------------------------------------------------------
+def mainMenu():
+    return('Please select one of the following:\n'
+           '- [A]dd a new account\n'
+           '- [V]iew existing accounts\n')
+
+# | accountDetails()
 # |---------------------------------------------------------------------------------
 # | Template to show structured layout of 'non-confidential' details of an account.
 # |-----------------------------------------------------------------------------
-def displayAccountDetails(type, owner, username):
+def accountDetails(type, owner, username):
     print('===================================================================\n',
           type,'-', owner, '\n',
           '-----------------------------------------------------------------\n',
