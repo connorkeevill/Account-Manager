775d483802065fa41817247e4508e6a78d03d236
Connor  Keevill <connor.keevill.19@gmail.com>
2017-02-14 23:02:03 +0000

    Add inputOption() method to helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -1,2 +1,17 @@
 #CK
 
+# | inputOption()
+# |--------------------------------------------------------------------------
+# | Returns a character based on the input from the prompt if it is in the
+# | options[] param. To be used to get the right input when getting the
+# | user to make a choice from multiple options. The characters in
+# | list passed for options NEED to be uppercase for them to be
+# | recognised when compared to the user's inputted choice.
+# |----------------------------------------------------
+def inputOption(prompt, options):
+    option = input(prompt)
+    while option.upper() not in options:
+        print('Make sure your choice is one of: ', ', '.join(options))
+        option = input(prompt)
+
+    return option
