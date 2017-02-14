b3252da828f4ffa93e898c832d0b6d5de3acc886
Connor  Keevill <connor.keevill.19@gmail.com>
2017-02-14 23:16:21 +0000

    Add main program loop to and code to get details for new account, and to allow user control of viewing accounts or adding a new one. Main loop currently is an infinite loop due to lack of break statement

--- src/main.py
+++ src/main.py
@@ -2,19 +2,33 @@
 
 from Account import Account
 from FileHandler import FileHandler
-import templates
+import templates, helpers
 
 file = FileHandler('accounts.txt')
 
-#Insert code for selection of programs function:
+print(templates.greeting())
 
+# |-------------------------------------------
+# | Main loop for the program while running.
+# |--------------------------------------
+while True:
+    file.getAccounts()
+    choice = helpers.inputOption(templates.mainMenu(), ['A', 'V'])
 
+    if choice == 'V':
+        for account in file.accounts:
+            templates.accountDetails(account.type, account.owner, account. username)
 
+    elif choice == 'A':
+        # |------------------------------------------
+        # | Getting the details of the new account.
+        # |--------------------------------------
+        type = input('Enter the type of account this is: ')
+        owner = input('Enter the name of the owner of the account: ')
+        username = input('Enter the username of the account: ')
+        password = input('Enter the account\'s password: ')
+        key = input('!!IMPORTANT!! Enter the key that the password will be encrypted with:')
+        account = Account(1, type, owner, username, password)
+        account.encrypt(key)
 
-#Displaying accounts w/out password
-for account in file.accounts:
-    templates.displayAccountDetails(account.type, account.owner, account. username)
-
-
-
-
+        file.addAccount(account)
