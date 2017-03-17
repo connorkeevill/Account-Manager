ab448cca966f4730bccd175c4272cd81d1b4b022
Connor Keevill <connor.keevill.19@gmail.com>
2017-03-17 10:36:42 +0000

    Make mainMenu() and add empty dependent methods

--- src/interface.py
+++ src/interface.py
@@ -1,14 +1,34 @@
 #CK
 
-import templates
+import templates, helpers
 
 class CommandLine():
     def __init__(self, file):
+        self.file = file
+
+        self.greet()
         self.mainMenu()
 
-
-    def mainMenu(self):
+    def greet(self):
         print(templates.greeting())
 
+    def mainMenu(self):
+        choice = helpers.inputOption(templates.mainMenu(), ['A', 'V', 'Q'])
+
+        if choice == 'A':
+            self.addAccount()
+        elif choice == 'V':
+            self.viewAccounts()
+        elif choice == 'Q':
+            self.quit()
+
+    def addAccount(self):
+        None
+
+    def viewAccounts(self):
+        None
+
+    def quit(self):
+        None
 
 
